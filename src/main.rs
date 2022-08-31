1d9e4c40acfe47f722ba5b384de6cff9ad0218b3
Newton Ni <nwtnni@gmail.com>
2022-08-30 19:11:06 -0500

    Parse project as command-line argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::str::FromStr;
+
+use anyhow::anyhow;
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(about)]
+struct Command {
+    /// Project to grade (one of p1, p2, ..., p6)
+    project: Project,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum Project {
+    /// Lexer (lexanc.c)
+    P1,
+}
+
+impl FromStr for Project {
+    type Err = anyhow::Error;
+    fn from_str(project: &str) -> Result<Self, Self::Err> {
+        match project {
+            "1" | "p1" | "P1" => Ok(Project::P1),
+            _ => Err(anyhow!("Invalid project `{}`", project)),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let command = Command::parse();
+
+    match command.project {
+        Project::P1 => todo!(),
+    }
 }
